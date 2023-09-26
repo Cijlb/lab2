975f9ca2f5b08a564f2bb6d35d784acbe5b20041
Cilb <81385156+Cijlb@users.noreply.github.com>
2023-09-26 13:49:27 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ fun main(args: Array<String>) {
     println("Hello World 2!")
     println("Hello World 3!")
 
+    println("Віддалена зміна!!")
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
